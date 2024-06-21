fb91529df896c4905368abadc1116ba3cf105039
Mohamed <mohammedmohamuud.01@gmail.com>
2024-06-22 00:32:40 +0300

    ended the registration

---
+++ lib/Src/Components/button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+  const CustomButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 0, 112, 197),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Src/Screens/registration_screen.dart
+++ lib/Src/Screens/registration_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:news_app/Src/Components/button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:news_app/Src/Components/text_field.dart';
 
 class RegistrationScreen extends StatelessWidget {
@@ -11,11 +14,59 @@ class RegistrationScreen extends StatelessWidget {
     final emailFormKey = GlobalKey<FormState>();
     final passwordFormKey = GlobalKey<FormState>();
 
+    Future<void> signInWithEmailAndPassword() async {
+      try {
+        print("Trying");
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim(),
+        );
+        // Navigate to the next screen after successful sign-in
+        // Navigator.pushReplacement(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (context) => ReportScreen(),
+        //   ),
+        // );
+        print("Successfully signed in");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Registared Succefully"),
+          ),
+        );
+      } catch (e) {
+        // Show error message if sign-in fails
+        print("Failed to sign in: $e");
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Failed to sign in: ${e.toString()}"),
+            ),
+          );
+        }
+      }
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Text(
+              "Register your account now",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            const Text(
+              "In order to get the latest news!",
+              style: TextStyle(
+                fontSize: 17,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
             CustomTextFeild(
               hintText: "email@example.com",
               controller: emailController,
@@ -31,6 +82,13 @@ class RegistrationScreen extends StatelessWidget {
               formkey: passwordFormKey,
               obsecureText: true,
             ),
+            const SizedBox(
+              height: 8,
+            ),
+            CustomButton(
+              text: "Register",
+              onTap: () => signInWithEmailAndPassword(),
+            )
           ],
         ),
       ),
